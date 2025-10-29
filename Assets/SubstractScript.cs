f0944694f431c63c32f4576aeba39daa133c0432
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-10-29 20:18:25 +0300

    fix #4

--- Assets/SubstractScript.cs
+++ Assets/SubstractScript.cs
@@ -6,17 +6,13 @@ public class SubstractScript : MonoBehaviour
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	private void OnTriggerEnter(Collider other)
 	{
-		var sverlo = other.gameObject;
-		var cube = gameObject;
-		var transf = cube.transform;
-		Model result = CSG.Subtract(cube, sverlo);
-		
-		gameObject.GetComponent<MeshFilter>().mesh = result.mesh;
-		gameObject.GetComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
-		cube.transform.position = transf.position;
-		cube.transform.rotation = transf.rotation;
-		
-		gameObject.GetComponent<SubstractScript>().enabled = false;
-		//gameObject.GetComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+		var subtracted = other.gameObject;
+		var sourceComponent = gameObject;
+		var transf = sourceComponent.transform;
+		Model result = CSG.Subtract(sourceComponent, subtracted);
+		sourceComponent.transform.position = new Vector3(0, 0, 0);
+		sourceComponent.GetComponent<MeshFilter>().sharedMesh = result.mesh;
+		sourceComponent.GetComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+		sourceComponent.transform.position = transf.position;
 	}
 }
