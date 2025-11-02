ea84ef3d9e38ddbbcf61c6ee27b538c69a9986ba
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-11-02 13:12:56 +0300

    finally works

--- Assets/Scripts/SubstractScript.cs
+++ Assets/Scripts/SubstractScript.cs
@@ -9,17 +9,25 @@ public class SubstractScript : MonoBehaviour
 	{
 		var subtracted = other.gameObject;
 		var sourceComponent = gameObject;
-		var transformLocalPos = sourceComponent.transform.localPosition;
+		var offset = sourceComponent.transform.localPosition;
         var transformLocalRot = sourceComponent.transform.localRotation;
         var transformLocalSc = sourceComponent.transform.localScale;
 
         Model result = CSG.Subtract(sourceComponent, subtracted);
-		sourceComponent.transform.position = new Vector3(0, 0, 0);
-		sourceComponent.GetComponent<MeshFilter>().sharedMesh = result.mesh;
+		//sourceComponent.transform.position = new Vector3(0, 0, 0);
+		Mesh mesh = result.mesh;
+		Vector3[] verts = mesh.vertices;
+		for (int i = 0; i < verts.Length; i++)
+			verts[i] -= offset;
+		mesh.vertices = verts;
+		mesh.RecalculateBounds();
+		sourceComponent.GetComponent<MeshFilter>().sharedMesh = mesh;
 		sourceComponent.GetComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
 
-		sourceComponent.transform.localPosition = transformLocalPos;
+
+
+		/*sourceComponent.transform.localPosition = transformLocalPos;
         sourceComponent.transform.localRotation = transformLocalRot;
-        sourceComponent.transform.localScale = transformLocalSc;
+        sourceComponent.transform.localScale = transformLocalSc;*/
     }
 }
