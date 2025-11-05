d19841217cbe52224f03c3562dc48ea9fa5e4bd2
Gesporidgers <konkord302@gmail.com>
2025-11-05 17:07:49 +0300

    fix angle logic (radians instead of angles)

--- Assets/Scripts/AngleRotation.cs
+++ Assets/Scripts/AngleRotation.cs
@@ -8,34 +8,41 @@ public class AngleRotation : MonoBehaviour
     public float minAngleValue;
     [SerializeField]
     public float maxAngleValue;
-    public float minDistanceValue;
+    
+    private float minDistanceValue;
     [SerializeField]
     public float maxDistanceValue;
 
     private float dirivative;
 
+    int counter = 0;
+
 
     private bool IsActive = false;
 
-    void StartAngleRecalculation()
+    public void StartAngleRecalculation()
     {
         IsActive = true;
         minDistanceValue = MovingObject.transform.localPosition.y;
     }
 
-    void StopAngleRecalculation()
+    public void StopAngleRecalculation()
     {
         IsActive = false;
     }
 
     private void FixedUpdate()
     {
+        counter++;
         if (IsActive)
         {
             float angle = transform.rotation.z - minAngleValue;
+            Debug.Log(transform.rotation.z);
             float distance = maxDistanceValue * (angle / (maxAngleValue - minAngleValue));
-            MovingObject.transform.localPosition.Set(MovingObject.transform.localPosition.x, minDistanceValue - distance, MovingObject.transform.localPosition.z);
+            Debug.Log("delta: " + distance);
+            MovingObject.transform.position = new Vector3(MovingObject.transform.localPosition.x, minDistanceValue - distance, MovingObject.transform.localPosition.z);
         }
+        if (counter > 20) counter = 0;
     }
 
     private void Start()
